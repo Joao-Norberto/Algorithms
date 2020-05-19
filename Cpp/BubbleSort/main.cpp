af7f373ab193185b3cabd7eccf7de3361bcbe3e5
Joao-Norberto <joaopedronorbertodasilva@gmail.com>
2020-05-19 17:46:53 -0300

    this'll crash your computer

--- Cpp/BubbleSort/main.cpp
+++ Cpp/BubbleSort/main.cpp
@@ -1,16 +1,41 @@
 #include <iostream>
 
-int main(int argc, const char* argv[])
+int main(int argc, const char *argv[])
 {
     int numbers[] = {5, 9, 3, 1, 2, 8, 4, 7, 6};
-    int numbers_size = sizeof(numbers)/sizeof(int), aux;
-    if (numbers[numbers_size] < numbers[numbers_size-1])
+    int vector_indexSize = (sizeof(numbers) / sizeof(int)) - 1, vector_indexOrdered, aux, num_lowest;
+
+    for (vector_indexOrdered = 0; vector_indexOrdered <= vector_indexSize; vector_indexOrdered++)
     {
-        aux = numbers[numbers_size];
-        numbers[numbers_size] = numbers[numbers_size-1];
-        numbers[numbers_size-1] = aux;       
+        for (int i = vector_indexSize; i >= vector_indexOrdered; i--)
+        {
+            if (i == vector_indexSize) {
+                numbers[i] <= numbers[i - 1] ? num_lowest = numbers[i] : num_lowest = numbers[i - 1];
+            }
+            else
+            {
+                if (numbers[i] < num_lowest) {
+                    num_lowest = numbers[i];
+                }
+                else if (numbers[i - 1] < num_lowest) {
+                    num_lowest = numbers[i - 1];
+                }
+            }
+
+            if (numbers[i] < numbers[i - 1])
+            {
+                aux = numbers[i];
+                numbers[i] = numbers[i - 1];
+                numbers[i - 1] = aux;
+            }
+        }
+        if (num_lowest != numbers[vector_indexOrdered]) {
+            vector_indexOrdered--;
+            std::cout << vector_indexOrdered << std::endl;
+        }
     }
-    for(int i = 0; i < sizeof(numbers)/sizeof(int); i++)
+
+    for (int i = 0; i < sizeof(numbers) / sizeof(int); i++)
     {
         std::cout << numbers[i] << ", ";
     }
