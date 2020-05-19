553fcefc541bd63ba8f2d493909af3fbe7a94fc2
Joao-Norberto <joaopedronorbertodasilva@gmail.com>
2020-05-18 23:16:41 -0300

    validações...

---
+++ Cpp/BubbleSort/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(int argc, const char* argv[])
+{
+    int numbers[] = {5, 9, 3, 1, 2, 8, 4, 7, 6};
+    int numbers_size = sizeof(numbers)/sizeof(int), aux;
+    if (numbers[numbers_size] < numbers[numbers_size-1])
+    {
+        aux = numbers[numbers_size];
+        numbers[numbers_size-1] = numbers[numbers_size];
+    }
+    for(int i = 0; i < sizeof(numbers)/sizeof(int); i++)
+    {
+        std::cout << numbers[i] << ", ";
+    }
+}
