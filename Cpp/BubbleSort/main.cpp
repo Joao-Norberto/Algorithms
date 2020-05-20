3cb43018792295fda48639008b1bb36ef0ec5351
Joao-Norberto <joaopedronorbertodasilva@gmail.com>
2020-05-19 21:18:21 -0300

    v1.0

--- Cpp/BubbleSort/main.cpp
+++ Cpp/BubbleSort/main.cpp
@@ -1,27 +1,29 @@
 #include <iostream>
 
+void printOrder(int num[], int indexSize)
+{
+    for (int i = 0; i <= indexSize; i++)
+    {
+        std::cout << num[i];
+        i == indexSize ? std::cout << "." : std::cout << ", ";
+    }
+    std::cout << "\033[m" << std::endl;
+}
+
 int main(int argc, const char *argv[])
 {
-    int numbers[] = {5, 9, 3, 1, 2, 8, 4, 7, 6};
-    int vector_indexSize = (sizeof(numbers) / sizeof(int)) - 1, vector_indexOrdered, aux, num_lowest;
+    int numbers[] = {5, 4, 3, 2, 6, 7, 8, 1, 9, 10};
+    int vector_indexSize = (sizeof(numbers) / sizeof(int)) - 1, vector_indexOrdered, aux;
+    int numbers_lowest, numbers_biggest, numbers_middle[2];
+    bool isOrdered = false;
+
+    std::cout << "\033[1;31m";
+    printOrder(numbers, vector_indexSize);
 
     for (vector_indexOrdered = 0; vector_indexOrdered <= vector_indexSize; vector_indexOrdered++)
     {
         for (int i = vector_indexSize; i >= vector_indexOrdered; i--)
         {
-            if (i == vector_indexSize) {
-                numbers[i] <= numbers[i - 1] ? num_lowest = numbers[i] : num_lowest = numbers[i - 1];
-            }
-            else
-            {
-                if (numbers[i] < num_lowest) {
-                    num_lowest = numbers[i];
-                }
-                else if (numbers[i - 1] < num_lowest) {
-                    num_lowest = numbers[i - 1];
-                }
-            }
-
             if (numbers[i] < numbers[i - 1])
             {
                 aux = numbers[i];
@@ -29,14 +31,7 @@ int main(int argc, const char *argv[])
                 numbers[i - 1] = aux;
             }
         }
-        if (num_lowest != numbers[vector_indexOrdered]) {
-            vector_indexOrdered--;
-            std::cout << vector_indexOrdered << std::endl;
-        }
     }
-
-    for (int i = 0; i < sizeof(numbers) / sizeof(int); i++)
-    {
-        std::cout << numbers[i] << ", ";
-    }
+    std::cout << "\033[1;32m";
+    printOrder(numbers, vector_indexSize);
 }
